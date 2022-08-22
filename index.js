97d17260915ff8b1d12e9bc17d17c7c184fa1b78
Lesley Labady <labadylesley@gmail.com>
2022-08-22 13:08:03 -0400

    Done

--- index.js
+++ index.js
@@ -1,18 +1,42 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
-}
+  let result=[]
+  let numIndex= new Map();
 
+  for (let i=0;i<array.length;i++){
+    let num = array[i]
+    let com = target - num;
+
+    if(numIndex.has(com)){
+      result[0]=numIndex.get(com);
+      result[1]=i;
+      return result;
+    }
+    numIndex.set(num, i)
+  }
+  return result;
+}
 /* 
   Write the Big O time complexity of your function here
 */
 
 /* 
   Add your pseudocode here
+  1(in the array i need to wirte a function that pick the first number then add it self to each number in the array;
+    then moves to the next number in the array to add it self to each number of the array;
+    ect... then all the sum should be in a new array)
+  then with the sum of that number see if it === to the target number
+  if so return true else return false
+  2(in this new array if any of the number in the new array === to the target number return turn else return false)
 */
 
 /*
   Add written explanation of your solution here
+
 */
+    
+  
+  
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
